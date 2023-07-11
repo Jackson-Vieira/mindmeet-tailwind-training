8cb96817f6746099a7a1e8619e738865d0fdb3d7
jacksonsv <jacksonvieiira12@gmail.com>
2023-07-11 00:38:49 -0300

    base app component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,50 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+
+interface TabItem {
+  name: string;
+  component: string;
+}
+
+interface Components {
+  [key: string]: () => Promise< any >;
+}
+
+const components: Components = {
+  buttons: () => import("./components/buttons/ButtonsView.vue"),
+  alerts: () => import("./components/alerts/AlertsView.vue"),
+}
+
+const tabs: TabItem[] = [
+  {
+    name: "Buttons",
+    component: "buttons",
+  },
+  {
+    name: "Alerts",
+    component: "alerts",
+  }
+];
+
+const activeTab = ref(0);
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
+  <!-- HeaderApp -->
+  <!-- TabsList -->
+  <!-- TabItem -->
   <main>
-    <TheWelcome />
+    <span> Active tab index: {{  activeTab }}</span>
+    <ul>
+      <li v-for="(tab, index) in tabs" :key="index">
+        <p @click="activeTab = index">{{ tab.name }} </p>
+      </li>
+    </ul>
+    <component :is="components[tabs[activeTab].component]" />
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
