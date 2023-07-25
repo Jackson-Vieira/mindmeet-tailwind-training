8fb3a891f347d7e69d6deda11ea54dc6b0c013af
Jackson serafim <jacksonvieiira12@gmail.com>
2023-07-25 01:58:16 -0300

    export app module

---
+++ src/components/app/index.ts
@@ -0,0 +1,11 @@
+import Container from "./Container.vue";
+import Footer from "./Footer.vue";
+import Logo from "./Logo.vue";
+import Navbar from "./Navbar.vue";
+
+export {
+  Container,
+  Footer,
+  Logo,
+  Navbar,
+};
