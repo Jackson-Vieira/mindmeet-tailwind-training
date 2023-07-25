68f2d72b0e00c08aa1dcc0dd8081c7c009bb0178
Jackson serafim <jacksonvieiira12@gmail.com>
2023-07-25 01:58:47 -0300

    shared components

---
+++ src/components/buttons/DefaultButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button :class="ButtonClass">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { cva } from 'class-variance-authority'
+import { computed } from 'vue'
+
+const ButtonClass = computed(() =>
+  cva(
+    'inline-flex min-h-[32px] items-center justify-center rounded-md text-white hover:cursor-pointer',
+    {
+      variants: {
+        intent: {
+          primary: 'bg-blue-600 hover:bg-blue-500',
+          secondary: 'bg-gray-600 hover:bg-gray-500',
+          tertiary: 'bg-green-600 hover:bg-green-500'
+        },
+        size: {
+          large: 'px-8 py-3 text-lg font-bold',
+          medium: 'px-6 py-1 text-md font-semibold',
+          small: 'px-4 py-0.5 text-sm font-semibold'
+        }
+      }
+    }
+  )({
+    intent: 'primary',
+    size: 'medium'
+  })
+)
+</script>
+
+<style scoped></style>
---
+++ src/components/forms/Input.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <label
+      :for="props.id"
+      class="mb-2 block text-sm font-medium text-gray-900"
+      >{{ labelText }}</label
+    >
+    <input
+      :type="props.type"
+      :id="props.id"
+      class="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm"
+      :placeholder="props.placeholderText"
+      required
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+type InputType = 'text' | 'email' | 'password'
+
+const props = defineProps({
+  labelText: {
+    type: String,
+    required: true
+  },
+  id: {
+    type: String,
+    required: true,
+    validator: (value: string) => {
+      return value.trim().length > 0
+    }
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  placeholderText: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String as PropType<InputType>,
+    required: true,
+    validator: (value: string) => {
+      return ['text', 'email', 'password'].includes(value)
+    }
+  }
+})
+</script>
+
+<style scoped></style>
