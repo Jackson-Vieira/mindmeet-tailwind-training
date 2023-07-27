136d07e66cd2af34a72912be2cb5e3b22c28ca09
jacksonsv <jacksonvieiira12@gmail.com>
2023-07-27 16:12:19 -0300

    base conference layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import Home from '@/views/Home/index.vue'
-import Alert from './components/alerts/Alert.vue'
+// import Home from '@/views/Home/index.vue'
+// import Alert from './components/alerts/Alert.vue'
+import Conference from '@/views/Conference/index.vue'
 </script>
 
 <template>
-  <Home />
+  <Conference />
 </template>
 
 <style scoped></style>
---
+++ src/views/Conference/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex min-h-screen w-full flex-col overflow-hidden">
+    <main class="relative flex flex-1 bg-gray-600">
+      <div
+        class="absolute left-1/2 top-0 -translate-x-1/2 transform bg-black-dark"
+      >
+        Tempo
+      </div>
+      <div class="w-full bg-green-300">Pedro</div>
+      <div class="absolute bottom-0 right-0 aspect-video w-64 bg-red-400">
+        Jackson
+      </div>
+    </main>
+    <footer class="bg-gray-red hover:bg-red-600">
+      <div>Controls bar</div>
+    </footer>
+  </div>
+</template>
+
+<style scoped></style>
